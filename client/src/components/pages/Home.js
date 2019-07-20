ca7418db4d0ed5229dd09e3e09284bd78b2e34cd
PJ Mantoss <joelptoss@gmail>
2019-07-20 19:28:29 +0100

    Add client folder

---
+++ client/src/components/pages/Home.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Contacts from '../contacts/Contacts';
+import ContactForm from '../contacts/ContactForm';
+import ContactFilter from '../contacts/ContactFilter';
+
+const Home = () => {
+    return (
+        <div className="grid">
+            <div><Contact/></div>
+            <div><Contacts/></div>
+        </div>
+    )
+}
+
+export default Home
