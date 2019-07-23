07fd9b5274c6d68587f6a670d307409b307da9ac
PJ Mantoss <joelptoss@gmail>
2019-07-23 19:12:47 +0100

    Modified Register, Home and authReducer components

--- client/src/App.js
+++ client/src/App.js
@@ -9,6 +9,7 @@ import Alerts from './components/layout/Alerts';
 import ContactState from './context/contact/ContactState';
 import AuthState from './context/auth/AuthState';
 import AlertState from './context/alert/AlertState';
+import setAuthToken from './utils/setAuthToken';
 import './App.css';
 
 const App = () => {
